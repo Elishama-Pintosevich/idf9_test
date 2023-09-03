2c66a8d2b9bc0b7f376442a2d5b9fdc86ea2f389
Elishama-Pintosevich <127098904+Elishama-Pintosevich@users.noreply.github.com>
2023-09-03 11:59:19 +0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async(req,res) => {
-  res.json({msg:"express work!"})
+  res.json({msg:"express work! 12:00"})
 })
 
 module.exports = router;
